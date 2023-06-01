6e28682b49648c2659e55994b3e8f6d4cc77d06d
lawand-allo <lawand.allo@sap.com>
2023-06-02 00:33:45 +0200

    add unit tests for get order

---
+++ usecase/get_order_test.go
@@ -0,0 +1,44 @@
+package usecase
+
+import (
+	"cadence-demo/model"
+	"cadence-demo/usecase/dependency/mock"
+	"errors"
+	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetOrder(t *testing.T) {
+	mockController := gomock.NewController(t)
+	mockRepository := mock.NewMockOrderRepository(mockController)
+	getOrderUseCase := NewGetOrderUsecase(mockRepository)
+	someUUID := uuid.New()
+	someOrder := &model.Order{
+		OrderName: "burger",
+		State:     "completed",
+		PickUp:    true,
+	}
+	mockRepository.EXPECT().ReadOrder(someUUID).Times(1).Return(someOrder, nil)
+	expectedResponse := &model.GetOrderResponse{
+		OrderId: someUUID,
+		Order:   *someOrder,
+	}
+
+	response, err := getOrderUseCase.GetOrder(someUUID)
+	assert.NoError(t, err)
+	assert.Equal(t, expectedResponse, response)
+}
+
+func TestGetOrderNotFound(t *testing.T) {
+	mockController := gomock.NewController(t)
+	mockRepository := mock.NewMockOrderRepository(mockController)
+	getOrderUseCase := NewGetOrderUsecase(mockRepository)
+	someUUID := uuid.New()
+	expectedError := errors.New("order not found")
+	mockRepository.EXPECT().ReadOrder(someUUID).Times(1).Return(nil, expectedError)
+
+	_, err := getOrderUseCase.GetOrder(someUUID)
+	assert.ErrorContains(t, err, expectedError.Error())
+}
