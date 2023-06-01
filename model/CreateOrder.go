0e0ca34a8c3310d1063b10289f674a8dd1f618e1
lawand-allo <lawand.allo@sap.com>
2023-06-01 19:00:36 +0200

    feat(model): add Order object

--- model/CreateOrder.go
+++
@@ -1,7 +0,0 @@
-package model
-
-import "github.com/google/uuid"
-
-type CreateOrderRequest struct {
-	uuid uuid.UUID
-}
---
+++ model/model.go
@@ -0,0 +1,22 @@
+package model
+
+import "github.com/google/uuid"
+
+type CreateOrderRequest struct {
+	OrderName   string `json:"name" binding:"required"`
+	WaitingTime int    `json:"waitingTime" binding:"required"`
+	PickUp      bool   `json:"pickUp,omitempty""`
+}
+
+type GetOrderResponse struct {
+	OrderId   uuid.UUID `json:"orderId" binding:"required"`
+	OrderName string    `json:"name" binding:"required"`
+	State     string    `json:"state" binding:"required"`
+	PickUp    bool      `json:"pickUp" binding:"required"`
+}
+
+type Order struct {
+	OrderName string `json:"name" binding:"required"`
+	State     string `json:"state" binding:"required"`
+	PickUp    bool   `json:"pickUp" binding:"required"`
+}
